92cdf6672de3f8bf1cbb76d2acf1b906d584e7f7
fraolb <fraolbereket@gmail.com>
2024-04-10 21:54:15 +0300

    contract starter

---
+++ backend/contracts/SwapToken.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.7.0 <0.9.0;
+pragma abicoder v2;
+
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+
+contract SingleSwapToken {
+    ISwapRouter public constant SWAPROUTER =
+        ISwapRouter(0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E);
+
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    uint24 public constant feeTier = 3000;
+
+    function swapExactInputString(
+        uint amountIn
+    ) external returns (uint amountOut) {
+        // Transfer the specified amount of WETH9 to this contract.
+        TransferHelper.safeTransferFrom(
+            WETH9,
+            msg.sender,
+            address(this),
+            amountIn
+        );
+        // Approve the router to spend WETH9.
+        TransferHelper.safeApprove(WETH9, address(SWAPROUTER), amountIn);
+
+        // Note: To use this example, you should explicitly set slippage limits, omitting for simplicity
+        uint256 minOut = /* Calculate min output */ 0;
+        uint160 priceLimit = /* Calculate price limit */ 0;
+        // Create the params that will be used to execute the swap
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: WETH9,
+                tokenOut: DAI,
+                fee: feeTier,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: minOut,
+                sqrtPriceLimitX96: priceLimit
+            });
+        // The call to `exactInputSingle` executes the swap.
+        amountOut = SWAPROUTER.exactInputSingle(params);
+    }
+
+    function swapExactOutputSingle(
+        uint256 amountOut,
+        uint256 amountInMaximum
+    ) external returns (uint256 amountIn) {
+        // Transfer the specified amount of DAI to this contract.
+        TransferHelper.safeTransferFrom(
+            DAI,
+            msg.sender,
+            address(this),
+            amountInMaximum
+        );
+
+        // Approve the router to spend the specifed `amountInMaximum` of DAI.
+        // In production, you should choose the maximum amount to spend based on oracles or other data sources to acheive a better swap.
+        TransferHelper.safeApprove(DAI, address(SWAPROUTER), amountInMaximum);
+
+        ISwapRouter.ExactOutputSingleParams memory params = ISwapRouter
+            .ExactOutputSingleParams({
+                tokenIn: DAI,
+                tokenOut: WETH9,
+                fee: feeTier,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountOut: amountOut,
+                amountInMaximum: amountInMaximum,
+                sqrtPriceLimitX96: 0
+            });
+
+        // Executes the swap returning the amountIn needed to spend to receive the desired amountOut.
+        amountIn = SWAPROUTER.exactOutputSingle(params);
+
+        // For exact output swaps, the amountInMaximum may not have all been spent.
+        // If the actual amount spent (amountIn) is less than the specified maximum amount, we must refund the msg.sender and approve the SWAPROUTER to spend 0.
+        if (amountIn < amountInMaximum) {
+            TransferHelper.safeApprove(DAI, address(SWAPROUTER), 0);
+            TransferHelper.safeTransfer(
+                DAI,
+                msg.sender,
+                amountInMaximum - amountIn
+            );
+        }
+    }
+}
